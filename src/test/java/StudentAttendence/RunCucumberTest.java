5f7b994ff45f6edec9cffebc03384a8d30cb76b2
Amanda <Manda_Hansen@hotmail.com>
2021-09-16 16:58:55 +0200

    Initial commit

---
+++ src/test/java/StudentAttendence/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package StudentAttendence;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+
+public class RunCucumberTest{
+}
---
+++ src/test/java/StudentAttendence/StepDefinitions.java
@@ -0,0 +1,46 @@
+package StudentAttendence;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("^the student is attending the class$")
+    public void the_student_is_attending_the_class() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I fill in the student id$")
+    public void i_fill_in_the_student_id() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the student's attendence the given day should be registered$")
+    public void the_student_s_attendence_the_given_day_should_be_registered() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I check the attendence system$")
+    public void i_check_the_attendence_system() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I fill in the class and date$")
+    public void i_fill_in_the_class_and_date() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see a list of students who was present$")
+    public void i_should_see_a_list_of_students_who_was_present() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
